1c65f6fe94e04eed113e6d07d3bb3ba178f85662
Manjunadhan00 <manjusegar1968@gmail.com>
2024-11-07 15:07:19 +0530

    Update hello.sh
    I have beeen changed the code as per requirement pls upate in local frepo

--- hello.sh
+++ hello.sh
@@ -2,3 +2,7 @@
 echo "Helo Good Morning"
 echo "The file as been edited"
 echo "the file as been reedited"
+!/bin/bash
+echo "Helo Good Morning"
+echo "The file as been edited"
+echo "the file as been reedited"
